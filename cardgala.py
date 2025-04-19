8ada63853d721baa43df8459c9f5afa4397d9e02
Ethan Goldman <ethangoldman01@gmail.com>
2025-04-19 13:07:17 -0400

    Added argparse stuff back

--- cardgala.py
+++ cardgala.py
@@ -3,18 +3,12 @@ import argparse
 from client import Client
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--hostname', '--h')
-    # parser.add_argument("-d", required=False, default=False, action='store_true')
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hostname', '--h', default="127.0.0.1")
+    parser.add_argument("-d", required=False, default=False, action='store_true')
+    args = parser.parse_args()
 
-    # client = None
-
-    # # if args.hostname == "" or None:
-    #     raise Exception("--h flag requires a hostname argument")
-    # else:
-        # debug = args.d
-    client = Client("127.0.0.1", False)
+    client = Client(args.hostname, args.d)
     
     client.connect_to_server()
     client.run_client()
